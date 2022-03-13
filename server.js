9a4de6a1076fa4adc5dfa1e74d5a5717771eaee0
Toan Pham Phu <phamphutoan98@gmail.com>
2022-03-13 20:08:31 +0000

    Solution

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const upload = multer();
+
 var app = express();
 
 app.use(cors());
@@ -11,8 +14,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// Solution
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
